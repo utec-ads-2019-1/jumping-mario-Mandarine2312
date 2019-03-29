600b06f2bd8daccce37bea74db145a5656171ca3
Alejandro <alejandro.tang@utec.edu.pe>
2019-03-29 18:40:06 -0400

    Primer commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void countHighsLows(int* highs, int *lows, int startWall, int numOfWalls){
+    int nextWall;
+
+    for(int k = 0; k<numOfWalls-1; k++){
+        cin >> nextWall;
+
+        if(nextWall > startWall){
+            (*highs)++;
+        }else if(nextWall < startWall){
+            (*lows)++;
+        }
+
+        startWall = nextWall;
+    }
+}
+
 int main() {
+    int num, numOfWalls, startWall, highs, lows, caseCounter=1;
+
+    cin >> num;
+
+    for(int i = 0; i < num; i++){
+        highs = 0;
+        lows = 0;
+        cin >> numOfWalls;
+        cin >> startWall;
+
+
+        countHighsLows(&highs, &lows, startWall, numOfWalls);
+
+        cout << "Case " << caseCounter << ": " << highs << " " << lows << endl;
+
+        caseCounter++;
+    }
+
 	return EXIT_SUCCESS;
 }
 
